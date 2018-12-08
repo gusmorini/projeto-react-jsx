2a02f30db88f023fa2f6dc3fe1c9c5461127bf21
Gustavo Morini <gustavommh@gmail.com>
2018-12-08 10:51:38 -0200

    tarefas

--- src/pages/TarefasPage.js
+++ src/pages/TarefasPage.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import { Link, Route } from "react-router-dom";
+
+const TAREFAS = [
+  {
+    id: 1,
+    titulo: "Aprender React",
+    descricao: "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+  },
+  {
+    id: 2,
+    titulo: "Aprender Java Script",
+    descricao: "of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum'"
+  },
+  {
+    id: 3,
+    titulo: "Aprender React-Router",
+    descricao: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock"
+  }
+];
 
 class TarefasPage extends Component {
   render() {
+
+    const tarefasLinks = TAREFAS.map((tarefa, index) => {
+      return (
+        <li>
+          <Link to={'/tarefas/' + tarefa.id}> 
+            {tarefa.titulo} 
+          </Link>
+        </li>
+      )
+    });
+
     return (
       <div>
-        Tarefas Page
+        
+        <h1>Tarefas</h1>
+
+        <ul>{tarefasLinks}</ul>
+
       </div>
     )
   }
